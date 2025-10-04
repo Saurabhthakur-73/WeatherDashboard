81b5646e59520a9f46bd6402c96038e593be1410
Saurabhthakur-73 <162841055+Saurabhthakur-73@users.noreply.github.com>
2025-10-04 15:12:19 +0530

    Fix import path and update gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { StateContextProvider } from './Context/index.jsx'
+import { StateContextProvider } from './context/Index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StateContextProvider>
-    <App />
-  </StateContextProvider>,
+    <App/>
+  </StateContextProvider>
 )
